a8f20de0433c6f79b5016c7f9ef94696dc135a6c
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-02-17 12:53:33 +0500

    more code including javascript

--- script.js
+++ script.js
@@ -3,9 +3,41 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from('#nav', {
+        y: '-10',
+        opacity: 0,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to('.boundingelem', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: .2
+    })
+    .from('#herofooter', {
+        y: -10,
+        opacity: 0,
+        duration: 2,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+
+function circleChaptaKaro(){
+    window.addEventListener("mousemove", function(dets){
+        
+    })
+}
+
 function circleMouseFollower(){
     window.addEventListener("mousemove", function(dets){
         document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
     });
 }
 circleMouseFollower()
+firstPageAnim()
